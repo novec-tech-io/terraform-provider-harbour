eec18eab4edc38bebff0ea10fab1e62a6e8ad9eb
Flynn Tynan <flynntynan16@gmail.com>
2026-08-20 16:20:39 +0100

    adding additional info in regard to region selection.

--- examples/provider/main.tf
+++ examples/provider/main.tf
@@ -8,6 +8,8 @@ terraform {
 }
 
 provider "harbour" {
+  # eu-west-1 is Harbour's default region, but any AWS region is supported —
+  # substitute your deployment's actual region below.
   endpoint = "https://<api-id>.execute-api.eu-west-1.amazonaws.com"
   region   = "eu-west-1"
   role_arn = "arn:aws:iam::<account-id>:role/harbour-customer-prod"
